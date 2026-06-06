230fd02af073a8742ddb4452734e43a6dfd4f5df
jamcurt7 <jamesc.emerald@gmail.com>
2026-06-06 14:26:13 -0400

    Create 5_Settings.py

---
+++ pages/5_Settings.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Settings", page_icon="⚙️", layout="wide")
+
+st.title("Settings")
+
+st.info("This page will allow editing training maxes, bodyweight goal, and app settings.")
