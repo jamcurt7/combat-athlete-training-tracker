fc8ab2cc345474dde62f53b0798e576bfe214a10
jamcurt7 <jamesc.emerald@gmail.com>
2026-06-06 14:25:59 -0400

    Create 4_Export_Center.py

---
+++ pages/4_Export_Center.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Export Center", page_icon="📥", layout="wide")
+
+st.title("Export Center")
+
+st.info("This page will allow Excel and PNG chart downloads.")
